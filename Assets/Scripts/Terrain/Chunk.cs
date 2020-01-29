897c1766df76faabf69d2369824370dac7bda4e2
Тимур <timik44452@gmail.com>
2020-01-29 18:01:32 +0300

    Some changes

--- Assets/Scripts/Terrain/Chunk.cs
+++ Assets/Scripts/Terrain/Chunk.cs
@@ -110,7 +110,7 @@ public class Chunk : MonoBehaviour
 
                 //if (types[x, z] == 2)
                 //{
-                //    for (int i = 0; i < 2;i ++)
+                //    for (int i = 0; i < 2; i++)
                 //    {
                 //        Vector3 center = (v0 + v1 + v2 + v3) * 0.25F;
 
